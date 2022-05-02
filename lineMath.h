04a9dddf7738d5c477e41ef07d3d5a6e6fc1a9b2
EyalLitvin <eyalitvin@gmail.com>
2022-05-02 12:00:05 +0300

    updated getLine to return lots of points along the line

--- lineMath.h
+++ lineMath.h
@@ -11,4 +11,6 @@ using std::vector;
 #define LEFT_UP float3{-0.4748, 0.2777, 0.7154}
 #define RIGHT_DOWN float3{0.5287, 0.2777, 0.3472}
 
-vector<float3> getLine(float3 baseBoard, float3 XBoard, float3 YBoard, float2 pos1_2D, float2 pos2_2D);
+
+vector<float3> getNewBasis(float3 baseBoard, float3 XBoard, float3 YBoard)
+vector<float3> getLine(float3 baseBoard, vector<float3> basis, float2 pos1_2D, float2 pos2_2D, int numOfPoints)
