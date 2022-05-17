c038daaa3992a5aa2f3477e4ce3e7eee19b060a8
harel5595 <harel.w.1234@gmail.com>
2022-05-17 17:32:01 +0300

    circle is working!

--- lineMath.cpp
+++ lineMath.cpp
@@ -11,7 +11,7 @@ vector<float3> getNewBasis(float3 baseBoard, float3 XBoard, float3 YBoard)
 	float3 relY = YBoard - baseBoard;
 	float3 e1 = (1 / length(relX)) * relX;
 	float3 e2 = relY - (dot(relY, e1) * e1);
-	e2 *= 1 - length(e2);
+	e2 /= length(e2);
 	float3 e3 = cross(e1, e2);
 	vector<float3> basis;
 	basis.push_back(e1);
@@ -32,7 +32,7 @@ float3 translateToBoardCoordinates(float3 v)
 
 float3 translateToRealCoordinates(float2 v)
 {
-	return glob_e1 * v.x + glob_e2 * v.y;
+	return (glob_e1 * v.x) + (glob_e2 * v.y);
 }
 
 vector<float3> getLine(float3 baseBoard, vector<float3> basis, float2 pos1_2D, float2 pos2_2D, int numOfPoints)
@@ -54,16 +54,13 @@ vector<float3> getLine(float3 baseBoard, vector<float3> basis, float2 pos1_2D, f
 
 vector<float3> getCircArc(float3 baseBoard, vector<float3> basis, float2 center, float rad, float alpha0, float angle, int numOfPoints)
 {
-	float3 e1 = basis[0];
-	float3 e2 = basis[1];
-
 	vector<float3> positions;
 	
 	for (float t = 0; t <= 1; t += (1.0 / numOfPoints))
 	{
 		float2 circ_offset = rad * float2{ cos(alpha0 + (t * angle)), sin(alpha0 + (t * angle)) };
 		float2 board_point = center + circ_offset;
-		positions.push_back(translateToRealCoordinates(board_point));
+		positions.push_back(baseBoard + translateToRealCoordinates(board_point));
 	}
 	return positions;
 
--- lineMath.h
+++ lineMath.h
@@ -9,10 +9,10 @@ using std::vector;
 
 #define LEFT_DOWN float3{-0.6899, 0.2889, 0.4014} 
 #define LEFT_UP float3{-0.4233, 0.2831, 0.8263}
-#define RIGHT_DOWN float3{0.6630,0.2900,0.4105}
+#define RIGHT_DOWN float3{0.6630,0.2985,0.4105}
 
 
 vector<float3> getNewBasis(float3 baseBoard, float3 XBoard, float3 YBoard);
 vector<float3> getLine(float3 baseBoard, vector<float3> basis, float2 pos1_2D, float2 pos2_2D, int numOfPoints);
 float3 translateToBoardCoordinates(float3 v);
-vector<float3> getCircArc(float3 baseBoard, vector<float3> basis, float2 center, float rad, float alpha0, float angle, int numOfPoints)
+vector<float3> getCircArc(float3 baseBoard, vector<float3> basis, float2 center, float rad, float alpha0, float angle, int numOfPoints);
