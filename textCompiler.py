c9579a0c72ab0772ee12e6135c806a0dba6e12e0
harel5595 <harel.w.1234@gmail.com>
2022-09-04 15:54:28 +0300

    update

--- textCompiler.py
+++ textCompiler.py
@@ -303,9 +303,9 @@ def compileText(text, scale, X, Y, lineLengthLimit, lineAmountLimit, spacing, pa
 
 if __name__ == '__main__':
     text = 'HELLO FROM THE OTHER SIDE I MUST HAVE CALLED A THOUSAND TIMES'
-    print(cutIntoLines(text, 1, 8))
+    print(cutIntoLines(text, 0.05, 0.4))
     #compileWord('HELLO', 1, 10, 40, 'MyTrialFile.txt') # 8, 10, 0.5,
-    compileText(text, scale=0.05, X=-0.05, Y=0.23, lineLengthLimit=20, lineAmountLimit=10, spacing=0.005, path='hello.txt') #
+    compileText(text, scale=0.05, X=-0.05, Y=0.23, lineLengthLimit=0.4, lineAmountLimit=10, spacing=0.01, path='hello.txt') #
 
 
 
